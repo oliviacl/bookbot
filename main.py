ddd3a2c0c04180c1229f6db69676a3153e6beae7
Olivia C. <olivialngc1@gmail.com>
2025-03-07 16:29:14 +0100

    feat: pass book path and print report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, char_count_map
+import sys
+from stats import get_num_words, char_count_map, sort_characters
 
 
 def get_book_text(filepath):
@@ -7,11 +8,31 @@ def get_book_text(filepath):
 
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    print(char_count_map(text))
+    char_counts = char_count_map(text)
+    sorted_char_counts = sort_characters(char_counts)
+
+    print_report(book_path, num_words, sorted_char_counts)
+
+
+def print_report(book_path, num_words, char_counts):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at  {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_counts:
+        char, count = item["char"], item["count"]
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,17 @@ def char_count_map(content):
             chars[lowered] += 1
 
     return chars
+
+
+def sort_characters(char_dict):
+    char_list = []
+
+    for char, count in char_dict.items():
+        char_list.append({"char": char, "count": count})
+
+    def sort_on(dict):
+        return dict["count"]
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
