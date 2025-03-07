7ec1739403bc60ce0483a0109d2644246eeecf9d
Olivia C. <olivialngc1@gmail.com>
2025-03-07 16:12:14 +0100

    feat: add char count

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from stats import get_num_words
+from stats import get_num_words, char_count_map
 
 
 def get_book_text(filepath):
     with open(filepath) as f:
-        file_content = f.read()
-        return file_content
+        return f.read()
 
 
 def main():
-    book_content = get_book_text("./books/frankenstein.txt")
-    num_words = get_num_words(book_content)
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(char_count_map(text))
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_num_words(content):
     words = content.split()
     return len(words)
+
+
+def char_count_map(content):
+    chars = {}
+    for char in content:
+        lowered = char.lower()
+        if lowered not in chars:
+            chars[lowered] = 1
+        else:
+            chars[lowered] += 1
+
+    return chars
