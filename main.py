bd1ea9f07d7a10a34285afdd6f1836a975b387b5
Olivia C. <olivialngc1@gmail.com>
2025-03-07 16:06:31 +0100

    feat: get num words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+from stats import get_num_words
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_content = f.read()
+        return file_content
+
+
+def main():
+    book_content = get_book_text("./books/frankenstein.txt")
+    num_words = get_num_words(book_content)
+    print(f"{num_words} words found in the document")
+
+
+main()
